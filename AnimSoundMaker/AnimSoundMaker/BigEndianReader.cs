b907e4f32bdffc9299876daae29856ea1c50b063
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-02-18 20:32:06 +0100

    [gab-lib-ela] BigEndianReader > Remove unused

--- AnimSoundMaker/AnimSoundMaker/BigEndianReader.cs
+++ AnimSoundMaker/AnimSoundMaker/BigEndianReader.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Windows.Documents;
 using static System.BitConverter;
 using static System.Text.Encoding;
 

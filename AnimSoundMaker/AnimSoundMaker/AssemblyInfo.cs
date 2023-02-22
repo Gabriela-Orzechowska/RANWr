01660f1dfbfbf93a0a4d85f525943f6f5ec3a87b
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-02-22 09:53:51 +0100

    [Project] Settings and Assembly Info

--- AnimSoundMaker/AnimSoundMaker/AssemblyInfo.cs
+++ AnimSoundMaker/AnimSoundMaker/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -7,4 +9,23 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
+
 )]
+
+[assembly: AssemblyCompany("Mivaana")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("Editor for Revolution .brasd files")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("RANWr AnimSoundMaker")]
+[assembly: AssemblyTitle("RANWr AnimSoundMaker")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/Gabriela-Orzechowska/RANWr/")]
+[assembly: AssemblyTrademark("Mivaana")]
+[assembly: System.Resources.NeutralResourcesLanguage("en")]
+[assembly: System.Runtime.Versioning.TargetPlatform("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("Windows7.0")]
+[assembly: Guid("e306a59a-a7af-4b69-ab76-63006f0178c0")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: ComVisible(false)]
+

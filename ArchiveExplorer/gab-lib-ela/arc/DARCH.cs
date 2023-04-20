79074e25c7d20a67795b770297709830daaa471d
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-04-20 15:08:09 +0200

    [ArchiveExplorer] gablibela > YAZ0, DARCH

---
+++ ArchiveExplorer/gab-lib-ela/arc/DARCH.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using gablibela.io;
+using static gablibela.arc.DARCH;
+
+namespace gablibela
+{
+    namespace arc
+    {
+        public class DARCH
+        {
+            public static readonly UInt32 Signature = 0x55AA382D; //DARCH Signature
+
+            [StructLayout(LayoutKind.Sequential)]
+            public unsafe struct Header
+            {
+                public UInt32 Signature;
+                public UInt32 NodeStart;
+                public UInt32 NodeSize;
+                public UInt32 DataStart;
+                public fixed UInt32 Reserved[4];
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public unsafe struct NodeData
+            {
+                public byte isDir;
+                public byte stringPool1;
+                public byte stringPool2;
+                public byte stringPool3;
+                public UInt32 DataStartOrParent;
+                public UInt32 SizeOrEndNode;
+            }
+
+            public class Node
+            {
+                public bool IsDir;
+                public int Index;
+                public UInt32 StringPoolOffset;
+                public string Name;
+                public UInt32 DataStart;
+                public UInt32 DataSize;
+
+                public UInt32 ParentIndex;
+                public UInt32 EndNode;
+
+                public List<Node> Children;
+                public Node? Parent;
+
+                public byte[] Data;
+
+                public Node(NodeData data)
+                {
+                    this.IsDir = data.isDir > 0;
+                    if (!IsDir)
+                    {
+                        this.DataStart = data.DataStartOrParent;
+                        this.DataSize = data.SizeOrEndNode;
+                    }
+                    else
+                    {
+                        this.ParentIndex = data.DataStartOrParent;
+                        this.EndNode = data.SizeOrEndNode;
+                    }
+                    Children = new List<Node>();
+                }
+            }
+
+            public Header header;
+            public string name;
+
+            public NodeData[] rawNodeData;
+            public Node[] rawNodes;
+            public Node[] structure;
+
+            public byte[] data;
+
+            public DARCH(byte[] data) => new DARCH(data, "<null>.szs");
+
+            public DARCH(byte[] _data, string fileName)
+            {
+                this.data = _data;
+                this.name = fileName;
+                MemoryStream stream = new MemoryStream(data);
+                BetterBinaryReader reader = new BetterBinaryReader(stream);
+                header = reader.ReadStruct<Header>(0x00);
+
+                List<NodeData> _nodeData = new List<NodeData>();
+                List<Node> _nodes = new List<Node>();
+
+                NodeData rootNodeData = reader.ReadStruct<NodeData>(header.NodeStart);
+                Node rootNode = new Node(rootNodeData);
+
+                UInt32 nodeCount = rootNodeData.SizeOrEndNode - 1;
+
+                UInt32 stringTableOffset = header.NodeStart + rootNodeData.SizeOrEndNode * 0xC;
+                rootNode.Name = reader.ReadStringNT(stringTableOffset + GetStringPoolOffset(rootNodeData));
+
+                _nodeData.Add(rootNodeData);
+                _nodes.Add(rootNode);
+
+                for (int i = 0; i < nodeCount; i++)
+                {
+                    NodeData nodeRawData = reader.ReadStruct<NodeData>(header.NodeStart + (i+1) * 0xC);
+                    Node node = new Node(nodeRawData);
+                    node.Name = reader.ReadStringNT(stringTableOffset + GetStringPoolOffset(nodeRawData));
+                    node.Index = i + 1;
+                    _nodeData.Add(nodeRawData);
+                    _nodes.Add(node);
+                    if(!node.IsDir)
+                        node.Data = reader.ReadBytes((int)nodeRawData.SizeOrEndNode, nodeRawData.DataStartOrParent);
+
+
+                }
+                rawNodeData = _nodeData.ToArray();
+                rawNodes = _nodes.ToArray();
+
+                structure = ConvertToStructure(rawNodes);
+            }
+
+            public void PrintArchive()
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine($"Archive: {name}\n");
+                Console.ResetColor();
+
+                Console.WriteLine("   size   | SIGN | file/directory");
+                Console.WriteLine("---------------------------");
+
+                foreach(var node in rawNodes)
+                {
+                    if (node.Name == "") continue;
+
+                    Node testNode = node;
+                    List<string> names = new List<string>();
+                    while(testNode.Parent != null)
+                    {
+                        names.Insert(0, testNode.Name);
+                        testNode = testNode.Parent;
+                    }
+                    names.Append(node.Name);
+                    string sizeorsmth = node.IsDir ? "-----" : node.DataSize.ToString();
+                    string magic = ".DIR";
+                    if(!node.IsDir) magic = Encoding.UTF8.GetString(node.Data.Take(4).ToArray());
+                    magic = magic.Replace('\0', '.');
+                    Console.WriteLine($"{sizeorsmth.PadLeft(10)}  {magic.PadLeft(4)}  {string.Join("\\", names.ToArray())}");
+                }
+            }
+
+            private static Node[] ConvertToStructure(Node[] nodes)
+            {
+                List<Node> result = new();
+                List<Node> parentQueue = new();
+
+                foreach(Node node in nodes) 
+                {
+                    if (node.IsDir)
+                    {
+                        if (parentQueue.Count != 0)
+                        {
+                            
+                            parentQueue.Last().Children.Add(node);
+                            node.Parent = parentQueue.Last();
+                        }
+                        else result.Add(node);
+                        parentQueue.Add(node);
+                        
+                    }
+                    else
+                    {
+                        parentQueue.Last().Children.Add(node);
+                        node.Parent = parentQueue.Last();
+                    }
+
+                    if (parentQueue.Any(f => f.EndNode == node.Index + 1))
+                    {
+                        Node[] apply = parentQueue.FindAll(f => f.EndNode == node.Index + 1).ToArray();
+                        apply.Reverse();
+                        foreach (Node applyNode in apply)
+                        {
+                            
+                            parentQueue.Remove(applyNode);
+                        }
+                    }
+                }
+
+                return result.ToArray();
+            }
+
+            private static UInt32 GetStringPoolOffset(NodeData stringPoolBytes)
+            {
+                byte[] convPool = new byte[] { stringPoolBytes.stringPool3, stringPoolBytes.stringPool2, stringPoolBytes.stringPool1, 0x00, };
+                return BitConverter.ToUInt32(convPool);
+            }
+        }
+    }
+}
---
+++ ArchiveExplorer/gab-lib-ela/cmpr/YAZ0.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using gablibela.io;
+
+namespace gablibela
+{
+    namespace cmpr
+    {
+        public static class YAZ0
+        {
+            public static readonly string Signature = "Yaz0";
+            public static readonly UInt32 SignatureHex = 0x59617A30;
+            public static readonly byte DefaultCompressionLevel = 10;
+
+            public static byte[] Decode(byte[] source, bool force = false)
+            {
+                MemoryStream stream = new MemoryStream(source);
+                BetterBinaryReader reader = new BetterBinaryReader(stream);
+                if (!force)
+                {
+                    string _signature = reader.ReadString(4, 0);
+                    if (_signature != Signature) throw new Exception($"Invalid signature. Expected \"{Signature}\", found \"{_signature}\".");
+                }
+                UInt32 DecompressedSize = reader.ReadUInt32(0x4);
+
+                List<byte> data = new List<byte>();
+                reader.Seek(0x10);
+
+                while (data.Count < DecompressedSize)
+                {
+                    byte Code = reader.ReadByte();
+                    BitArray Flags = new BitArray(new byte[1] { Code });
+
+                    for (Int32 i = 7; i > -1; i--)
+                    {
+                        if (Flags[i] == true) data.Add(reader.ReadByte());
+                        else
+                        {
+                            if (data.Count >= DecompressedSize) break;
+
+                            byte ReadByte = reader.ReadByte();
+                            Int32 Offset = (((byte)(ReadByte & 0x0F) << 8) | reader.ReadByte()) + 1;
+                            Int32 Length = (ReadByte & 0xF0) == 0 ? reader.ReadByte() + 0x12 : (byte)((ReadByte & 0xF0) >> 4) + 2;
+
+                            for (Int32 j = 0; j < Length; j++) data.Add(data[data.Count - Offset]);
+                        }
+                    }
+                }
+                return data.ToArray();
+            }
+
+            public static byte[] Compress(byte[] source) => Compress(source, DefaultCompressionLevel);
+
+            public static byte[] Compress(byte[] source, byte compressionLevel)
+            {
+                List<byte> data = new List<byte>();
+                data.AddRange(Encoding.ASCII.GetBytes(Signature));
+                data.AddRange(BitConverter.GetBytes(source.Length).Reverse());
+                data.AddRange(new byte[8]);
+                data.AddRange(Encode(source, compressionLevel));
+
+                return data.ToArray();
+            }
+
+            //
+            // After hours of trying, finished by borrowing parts from  
+            // Yoinked from https://github.com/MichaelHinrichs/Switch-Toolbox/blob/master/Switch_Toolbox_Library/Compression/Formats/Yaz0.cs
+            // All credits here
+            //
+
+            public static byte[] Encode(byte[] source) => Encode(source, DefaultCompressionLevel);
+
+            public static byte[] Encode(byte[] source, byte compressionLevel)
+            {
+                UInt32 sourceLenght = (UInt32)source.Length;
+
+                UInt32 range = 0x1000; ;
+                if (compressionLevel == 0)
+                    range = 0;
+                else if (compressionLevel < 9)
+                    range = (UInt32)(0x10e0 * compressionLevel / 9 - 0x0e0);
+
+                UInt32 position = 0;
+                UInt32 sourceEnd = (UInt32)source.Length;
+
+                byte[] destination = new byte[sourceEnd + (sourceEnd + 8) / 8];
+                UInt32 destinationPosition = 0;
+                UInt32 codeBytePosition = 0;
+
+                UInt64 found = 0;
+                UInt64 numBytes = 0;
+                UInt32 diff;
+                Int32 maxLenght = 0x111;
+
+                while (position < sourceEnd)
+                {
+                    codeBytePosition = destinationPosition;
+                    destination[destinationPosition] = 0; destinationPosition++;
+
+                    for (Int32 i = 0; i < 8; i++)
+                    {
+                        if (position >= sourceEnd)
+                            break;
+
+                        numBytes = 1;
+
+                        if (range != 0)
+                        {
+                            UInt64 search = Search(source, position, maxLenght, range, sourceEnd);
+                            found = search >> 32;
+                            numBytes = search & 0xFFFFFFFF;
+                        }
+
+                        if (numBytes > 2)
+                        {
+                            diff = (UInt32)(position - found - 1);
+
+                            if (numBytes < 0x12)
+                            {
+                                destination[destinationPosition] = (byte)(diff >> 8 | (numBytes - 2) << 4); destinationPosition++;
+                                destination[destinationPosition] = (byte)(diff & 0xFF); destinationPosition++;
+                            }
+                            else
+                            {
+                                destination[destinationPosition] = (byte)(diff >> 8); destinationPosition++;
+                                destination[destinationPosition] = (byte)(diff & 0xFF); destinationPosition++;
+                                destination[destinationPosition] = (byte)((numBytes - 0x12) & 0xFF); destinationPosition++;
+                            }
+
+                            position += (UInt32)numBytes;
+                        }
+
+                        else
+                        {
+                            destination[codeBytePosition] |= (byte)(1 << (7 - i));
+                            destination[destinationPosition] = source[position]; destinationPosition++; position++;
+                        }
+                    }
+                }
+
+                byte[] result = new byte[destinationPosition];
+                Array.Copy(destination, result, destinationPosition);
+                return result;
+            }
+
+            public static UInt64 Search(byte[] source, UInt32 position, Int32 manLenght, UInt32 range, UInt32 sourceEnd)
+            {
+                UInt64 numBytes = 1;
+                UInt64 found = 0;
+
+                Int64 search;
+                UInt32 compareEnd, compare1, compare2;
+                byte c1;
+                UInt32 lenght;
+
+                if (position + 2 < sourceEnd)
+                {
+                    search = (position - range);
+                    if (search < 0)
+                        search = 0;
+
+                    compareEnd = (UInt32)(position + manLenght);
+                    if (compareEnd > sourceEnd)
+                        compareEnd = sourceEnd;
+
+                    c1 = source[position];
+                    while (search < position)
+                    {
+                        search = Array.IndexOf(source, c1, (Int32)search, (Int32)(position - search));
+                        if (search < 0)
+                            break;
+
+                        compare1 = (UInt32)(search + 1);
+                        compare2 = position + 1;
+
+                        while (compare2 < compareEnd && source[compare1] == source[compare2])
+                        {
+                            compare1++; compare2++;
+                        }
+
+                        lenght = compare2 - position;
+                        if (numBytes < lenght)
+                        {
+                            numBytes = lenght;
+                            found = (UInt32)search;
+                            if ((Int64)numBytes == manLenght)
+                                break;
+                        }
+                        search++;
+                    }
+                }
+                return ((found << 32) | numBytes);
+            }
+
+        }
+    }
+}
